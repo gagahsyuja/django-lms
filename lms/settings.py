c856aa811f9da9f5567e63df560db8fa478a9b1a
gagahsyuja <gghssa1@gmail.com>
2025-04-27 19:32:57 +0700

    Configure django to use Postgres

--- lms/settings.py
+++ lms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2gnt_w7*rh2-q6su6$^!85f_y^ju927azbcyn9maq-*2+hq&dp'
+SECRET_KEY = 'django-insecure-nj^7a=sfk8)^0p$4$e0__^adx#42n-tnwigt+(ifeo=4g0s&zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'postgres-db',
+        'USER': 'django',
+        'NAME': 'lms',
+        'PORT': 5432,
+        'PASSWORD': 'django'
     }
 }
 
